a0b877903319ea6199bb50552085a311bff15ba7
Craig Smith <craig.smith@768bit.com>
2018-12-01 01:07:59 +0000

    Added dep stuff as well as preparing for github

--- fs/bench/bench_create_test.go
+++ fs/bench/bench_create_test.go
@@ -5,9 +5,9 @@ import (
 	"os"
 	"testing"
 
-	"gitlab.768bit.com/vann/fuse"
-	"gitlab.768bit.com/vann/fuse/fs"
-	"gitlab.768bit.com/vann/fuse/fs/fstestutil"
+	"github.com/768bit/fuse"
+	"github.com/768bit/fuse/fs"
+	"github.com/768bit/fuse/fs/fstestutil"
 	"golang.org/x/net/context"
 )
 
